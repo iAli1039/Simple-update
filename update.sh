9f7b40c73107df659f1e4e722a84edbf74e1eec3
iAli1039 <ali.ufm1039@yahoo.com>
2024-03-31 23:59:59 +0300

    support ubuntu with the update packages and change keys

--- update.sh
+++ update.sh
@@ -7,25 +7,40 @@ while [ $i -lt 1 ]
 do
 echo "-------"
 echo " "
-echo "you to make update of course? (flathub y ,dnf n, Both b) or q for quit"
+echo "you to make update of course? (Flatpak packages f, System Packages s , Both b) or q for quit"
 
 read -rsn1 update
 
+DISTRO_UPDATE_COMMAND=""
+FOUND_DISTRO=$(uname -a | awk '{print $2}')
 
-if [ "$update" == "y" ]; 
+if [ "$FOUND_DISTRO" == "fedora" ]
 then
-	echo "Updating Flatpak ..."
+	DISTRO_UPDATE_COMMAND="sudo dnf update"
+elif [ "$FOUND_DISTRO" == "ubuntu" ]
+then
+	DISTRO_UPDATE_COMMAND="sudo apt update"
+else
+	echo "Distro is not supported yet, currently we only support fedora and ubuntu"
+	exit
+
+fi
+
+
+if [ "$update" == "f" ]; 
+then
+	echo "Updating Flatpak packages ..."
         flatpak update
 	i=1
-elif [ "$update" == "n" ]; 
+elif [ "$update" == "s" ]; 
 then
-	echo "Updating DNF ..."
-    	sudo dnf update
+	echo "Updating system packages ..."
+	$DISTRO_UPDATE_COMMAND
 	i=1
 elif [ "$update" == "b" ]; 
 then
 	echo "Updating both ..."
-   	sudo dnf update; flatpak update
+   	$DISTRO_UPDATE_COMMAND; flatpak update
 	i=1
 elif [ "$update" == "q" ];
 then
