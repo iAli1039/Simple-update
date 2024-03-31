4da871a6a6b61bc67bd2b33c6de1dc7e89760d7d
iAli1039 <ali.ufm1039@yahoo.com>
2024-03-31 22:37:16 +0300

    making things do stuff

--- update.sh
+++ update.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
+i="0"
 
-echo "you to make update of course? (flathub y ,dnf n, Both b)"
+echo "Hellooooooo olloooloo"
 
-read update
+while [ $i -lt 1 ]
+do
+echo "-------"
+echo " "
+echo "you to make update of course? (flathub y ,dnf n, Both b) or q for quit"
 
-if [ $update == "y" ]; then
+read -rsn1 update
+
+
+if [ "$update" == "y" ]; 
+then
+	echo "Updating Flatpak ..."
         flatpak update
-elif [ $update == "n" ]; then
+	i=1
+elif [ "$update" == "n" ]; 
+then
+	echo "Updating DNF ..."
     	sudo dnf update
-
-else    
-    sudo dnf update; flatpak update
+	i=1
+elif [ "$update" == "b" ]; 
+then
+	echo "Updating both ..."
+   	sudo dnf update; flatpak update
+	i=1
+elif [ "$update" == "q" ];
+then
+	echo "Quitting app now !"
+	i=1
+else
+	echo "Please, read the instructions, and enter a letter of the provided list next time, don't act stupid"
 
 fi
+done
